89136088dee58535e4b0d398f478e65749dee129
xannem <41248956+xannem@users.noreply.github.com>
2021-01-20 17:29:12 -0800

    frontend updates

---
+++ scriptReport.js
@@ -0,0 +1,10 @@
+
+
+function generateReport(){
+
+    let begin = document.getElementById('beginDate').value
+
+    let end = document.getElementById('endDate').value
+
+    alert("You've clicked generate button looking for " + begin + " to " + end)
+}
