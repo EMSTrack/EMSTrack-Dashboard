ae6e0acaa79fe167eb15026ccf39c9ab33e0e53c
xannem <41248956+xannem@users.noreply.github.com>
2021-03-03 15:20:46 -0800

    updates

--- aaa.py
+++ aaa.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-
 import plotly.express as px
 import pandas as pd
 import requests
@@ -16,21 +13,15 @@ import datetime
 import dateutil.parser
 import os
 import yaml
-
-
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import folium
 from folium import plugins
-
 import plotly.graph_objects as go  # or plotly.express as px
-
 from datetime import date
 import re
-
 # --- CSS FORMATTING
-
 main_colors = {
     'main-blue': '#343a40',
     'medium-blue-grey': 'rgb(77, 79, 91)',
@@ -43,22 +34,23 @@ main_colors = {
     'white': 'rgb(251, 251, 252)',
     'light-grey': 'rgb(208, 206, 206)'
 }
-
 main_container = {
     # 'margin': '0 auto',
     'margin-top': '30px',
     # 'background-color': main_colors['medium-green'],
-
 }
-
 color_red = {
     # 'background-color': main_colors['dark-pink-red'],
 }
-
 color_green = {
     # 'background-color': main_colors['medium-green'],
 }
-
+s_card_box ={
+    "maxHeight": "150px", 
+    "overflow": "auto",
+    "font-size":" 9px",
+    
+}
 s_button = {
     'margin-left': '15px',
     'min-width': '100px',
@@ -78,9 +70,7 @@ s_button = {
     'border-radius': ' .25rem',
     'background-color': main_colors['main-blue'],
     'border-color': '#343a40',
-
 }
-
 def get(url, params=None, extend=True):
     global base_url, token
     set_token()
@@ -88,11 +78,9 @@ def get(url, params=None, extend=True):
         url = base_url + url
     headers = {'Content-Type': 'application/json',
                'Authorization': f'Token {token}'}
-
     response = requests.get(url, params=params, headers=headers)
     response.raise_for_status()
     return response
-
 def post(url, params=None, extend=True):
     global base_url, token
     set_token()
@@ -100,20 +88,16 @@ def post(url, params=None, extend=True):
         url = base_url + url
     headers = {'Content-Type': 'application/json',
                'Authorization': f'Token {token}'}
-
     response = requests.post(url, headers=headers, data=params, timeout=10)
     response.raise_for_status()
     return response
-
 def set_token():
     global token
     global token_timestamp
-
     try:
         token_timestamp
     except NameError:
         token_timestamp = 0
-
     if token_timestamp == 0:
         param = {
             'username': cfg['username'],
@@ -125,40 +109,31 @@ def set_token():
         res.raise_for_status()
         token_timestamp = time.time()
         token = res.json()['token']
-
 def splitlotlan(row):
     row['lon'] = row['location']['longitude']
     row['lat'] = row['location']['latitude']
     if row['lat'] == 37.4219983 and row['lon'] == -122.084:
         return None
     return row
-
 # fig = px.line_mapbox(df, lat="lat", lon="lon", color="identifier", zoom=3, height=500)
 #
 # fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=4, mapbox_center_lat = 41,
 #     margin={"r":0,"t":0,"l":0,"b":0})
 #
 # fig.show()
-
 dict_ambulances = {}
-
 def init_dict_ambulances(start, end):
     ambulances = get_ambulances()
-
-
     for amb in ambulances:
-
         if amb["id"] not in dict_ambulances.keys():
             color_sample = generate_new_color()
-
-            dict_ambulances[amb["id"]] = {"name": amb["identifier"], "dict_calls": [], "ambulance_color": color_sample}
-
-
+            dict_ambulances[amb["id"]] = {
+                "name": amb["identifier"], "dict_calls": [], "ambulance_color": color_sample}
         data = get(
             f'ambulance/{amb["id"]}/updates/?filter={start}T15:00:00.000Z,{end}T15:00:00.000Z').json()
-        for d in data: # insert at beginning, since api call gives us positions from newest to oldest
-            dict_ambulances[amb["id"]]["dict_calls"].insert(0, {"location": d["location"], "timestamp": d["timestamp"]})
-
+        for d in data:  # insert at beginning, since api call gives us positions from newest to oldest
+            dict_ambulances[amb["id"]]["dict_calls"].insert(
+                0, {"location": d["location"], "timestamp": d["timestamp"]})
 def get_ambulances():
     global cfg
     global base_url
@@ -167,16 +142,13 @@ def get_ambulances():
     SERVER = 'UCSD'
     cfg = cfg[SERVER]
     base_url = cfg['url']
-
     res = get('ambulance')
     ambulances = res.json()
-
     return ambulances
-
 def get_ambulance_colors(ambulances):
     colors = list(main_colors.values())
     color_map = {}
-    i = 0 # cycle through all main colors
+    i = 0  # cycle through all main colors
     for ambulance in ambulances:
         id = ambulance['id']
         if id not in color_map:
@@ -186,7 +158,6 @@ def get_ambulance_colors(ambulances):
             else:
                 i = 0
     return color_map
-
 def find_center():
     center = {'lon': 0, 'lat': 0}
     total_sum_long = 0
@@ -199,13 +170,11 @@ def find_center():
             total_sum_lat += call['location']['latitude']
             total_calls_long += 1
             total_calls_lat += 1
-
     mean_long = total_sum_long/total_calls_long
     mean_lat = total_sum_lat/total_calls_lat
     center['lon'] = mean_long
     center['lat'] = mean_lat
     return center
-
 def get_fig(start, end):
     ambulances = get_ambulances()
     ids = [ambulance['id'] for ambulance in ambulances]
@@ -221,7 +190,7 @@ def get_fig(start, end):
         data = [{'id': id, 'identifier': ambulance['identifier'], **item}
                 for item in data]
         # if len(data) != 0:
-            # app.logger.info(data[0])
+        # app.logger.info(data[0])
         df = df.append(pd.DataFrame(data))
         # app.logger.info(df)
         # print(data)
@@ -242,14 +211,16 @@ def get_fig(start, end):
         # app.logger.info(data.loc[:step:stepsize, 'id'])
         # app.logger.info(colors)
         a = data.loc[:step:1]
+        amb_name = a['id']
         unique_ids = a.id.unique()
-        app.logger.info("number of ambulances in iter {}: {}".format(step, len(unique_ids)))
+        app.logger.info("number of ambulances in iter {}: {}".format(
+            step, len(unique_ids)))
         lons = []
         lats = []
         colors = []
         for id in unique_ids:
-            lons.append(a.loc[a['id']==id, 'lon'].iloc[-1])
-            lats.append(a.loc[a['id']==id, 'lat'].iloc[-1])
+            lons.append(a.loc[a['id'] == id, 'lon'].iloc[-1])
+            lats.append(a.loc[a['id'] == id, 'lat'].iloc[-1])
             colors.append(color_map[id])
         fig.add_trace(
             go.Scattermapbox(
@@ -257,10 +228,13 @@ def get_fig(start, end):
                 mode="markers",
                 lon=lons,
                 lat=lats,
-                marker=go.scattermapbox.Marker(symbol='circle', color=colors, size=14),
+                text=amb_name, textposition="bottom right",
+                marker=go.scattermapbox.Marker(
+                    symbol='circle', color=colors, size=14),
             )
         )
-
+        #  marker = {'size': 20, 'symbol':   "airport", 'color':dict_ambulances[id]['ambulance_color'] },
+        # marker={'size': 10, 'color':dict_ambulances[id]['ambulance_color']}
     # for id in dict_ambulances.keys():
     #     # for step in range(2, data.shape[0], 5):
     #     for i in range(0, len(dict_ambulances[id]['dict_calls']), 50): # need to fix later
@@ -274,9 +248,7 @@ def get_fig(start, end):
     #                 lon=sublist_long,
     #                 lat=sublist_lat,
     #                 marker={'size': 10, 'color':dict_ambulances[id]['ambulance_color']}))
-
     # center = find_center()
-
     fig.update_layout(
         margin={'l': 0, 't': 0, 'b': 0, 'r': 0},
         mapbox={
@@ -284,7 +256,6 @@ def get_fig(start, end):
             'style': "stamen-terrain",
             'zoom': 8}
     )
-
     fig.data[0].visible = True
     # app.logger.info(fig.data)
     steps = []
@@ -296,87 +267,65 @@ def get_fig(start, end):
         )
         step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
         steps.append(step)
-
     sliders = [dict(
         active=1,
         currentvalue={"prefix": "Frequency: "},
         pad={"t": 50},
         steps=steps
     )]
-
     fig.update_layout(
         sliders=sliders
     )
-
+    
+    
     # fig.show()
     # fig.write_html("plotly.html")
-
     return fig
-
 external_stylesheets = [dbc.themes.BOOTSTRAP]
-
 # external_stylesheets = [
 #     'https://codepen.io/chriddyp/pen/bWLwgP.css']
 # external_stylesheets = [
 #     'https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 ALLOWED_TYPES = (
     "text", "number", "password", "email", "search",
     "tel", "url", "range", "hidden",
 )
-
-
-
-
-def get_random_color(pastel_factor = 0.5):
-    return [(x+pastel_factor)/(1.0+pastel_factor) for x in [random.uniform(0,1.0) for i in [1,2,3]]]
-
-def color_distance(c1,c2):
-    return sum([abs(x[0]-x[1]) for x in zip(c1,c2)])
-
-def generateo_new_color(existing_colors,pastel_factor = 0.5):
+def get_random_color(pastel_factor=0.5):
+    return [(x+pastel_factor)/(1.0+pastel_factor) for x in [random.uniform(0, 1.0) for i in [1, 2, 3]]]
+def color_distance(c1, c2):
+    return sum([abs(x[0]-x[1]) for x in zip(c1, c2)])
+def generateo_new_color(existing_colors, pastel_factor=0.5):
     max_distance = None
     best_color = None
-    for i in range(0,100):
-        color = get_random_color(pastel_factor = pastel_factor)
+    for i in range(0, 100):
+        color = get_random_color(pastel_factor=pastel_factor)
         if not existing_colors:
             return color
-        best_distance = min([color_distance(color,c) for c in existing_colors])
+        best_distance = min([color_distance(color, c)
+                             for c in existing_colors])
         if not max_distance or best_distance > max_distance:
             max_distance = best_distance
             best_color = color
     return best_color
-
-
 def generate_new_color():
-        rand = lambda: random.randint(100, 255)
-        return '#%02X%02X%02X' % (rand(), rand(), rand())
-
-
-
+    def rand(): return random.randint(100, 255)
+    return '#%02X%02X%02X' % (rand(), rand(), rand())
 def generate_ambulance_card(ambulance_id):
-    # app.logger.info('ambulance ambulanc ideop')
     # app.logger.info(ambulance_id)
     # app.logger.info(dict_ambulances[ambulance_id])
-
-
-
-
-
     single_ambulance = dict_ambulances[ambulance_id]
-
-
     return dbc.Card([
         dbc.CardBody(
             [
                 html.H4(single_ambulance["name"],
                         className="card-title"),
-                html.P(
-                    str(single_ambulance["dict_calls"]),
-                    className="card-text",
-                ),
+                html.Div([
+                         html.P(
+                             str(single_ambulance["dict_calls"]),
+                             className="card-text", style={}
+                         ),
+                         ], style = s_card_box),
                 dbc.Button(
                     "Go", color="primary"),
             ]
@@ -385,34 +334,22 @@ def generate_ambulance_card(ambulance_id):
         className="m-3",
         # color = color_sample,
         style={"backgroundColor": single_ambulance["ambulance_color"]},
-
     )
-
     # return dbc.Button(children=str(team_shortName),
     #                   color="primary",
     #                   className="mr-1",
     #                   id=str(team_shortName))
-
-
 # app = dash.Dash(external_stylesheets=[])
-
 #  html.Div(className = "container-sm", children = [
 #     ]),
-
 app.layout = html.Div(children=[
     html.Div(className="container",  style=main_container, children=[
-
         html.H1(children='Mileage Report', className="mb-4"),
-
         html.H3(id='button-clicks'),
-
-
         dcc.Input(
             id='input-field',
             type='text',
         ),
-
-
         dbc.Row(
             [
                 html.Div([
@@ -432,55 +369,39 @@ app.layout = html.Div(children=[
             ],
             align="center", justify="center", className="mb-5", style={'color': main_colors['main-blue']}
         ),
+        html.Div(
+            [
+                dcc.Graph(
+                    id='map-graph',
+                    figure=get_fig(date(2019, 10, 1), date(2020, 10, 30))
+                ),
+            ],  
+            # align="center", justify="center", style=color_green
+        ),
         dbc.Row(
             [
                 dbc.Col([
-
-
-                           html.H3(children='Testing Ambulances header'),
-
-
-
-
+                    html.H3(children='Testing Ambulances header'),
                     html.Div(
                         ""),
-
                 ],  id='output-ambulances', width=6, style=color_red),
                 dbc.Col([
-
-                    dcc.Graph(
-                        id='map-graph',
-                        figure=get_fig(date(2019, 10, 1), date(2020, 10, 30))
-                    ),
-
                 ], width=6, style=color_green),
             ]
         ),
-
     ]),
-
 ]
-
 )
-
-
 @app.callback(
     dash.dependencies.Output('output-ambulances', 'children'),
     [dash.dependencies.Input('input-field', 'value')])
 def update_col(prop):
-
     hrm = dbc.Col(children=[generate_ambulance_card(i)
                             for i in dict_ambulances])
-
     # hm = generate_ambulance_card(dict_ambulances["8"])
-
     # hm = generate_ambulance_card(list(dict_ambulances.keys())[0])
-
     # app.logger.info(hrm)
-
     return hrm
-
-
 @app.callback(
     dash.dependencies.Output('map-graph', 'figure'),
     [dash.dependencies.Input('my-date-picker-range', 'start_date'),
@@ -505,6 +426,5 @@ def update_output(start_date, end_date, n_clicks):
     #     return 'Select a date to see it displayed here'
     # else:
     #     return string_prefix
-
 if __name__ == '__main__':
     app.run_server(debug=True)
