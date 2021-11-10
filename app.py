82a05999b9fddfa0a3e9bd66d5b8ffdbc2d5b31c
koumorikun <turbohuynh@gmail.com>
2021-11-10 14:34:01 -0800

    docker https

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ app = dash.Dash(__name__,
                 # server=server, 
                 external_stylesheets=external_stylesheets,
                 url_base_pathname='/dashboard/')
+app.logger.info('Test info')
+app.logger.info(app.config)
 
 # Defines the actual layout of HTML elements on the application
 app.layout = html.Div(children=[
