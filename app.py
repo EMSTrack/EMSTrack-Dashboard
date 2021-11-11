a072bdb667207afd99a2d8735460fa9dfdc5357a
koumorikun <turbohuynh@gmail.com>
2021-11-10 16:25:52 -0800

    docker https

--- app.py
+++ app.py
@@ -20,12 +20,13 @@ generate_n_clicks = False
 # Adds Bootstrap styling to application
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-server = Flask(__name__)
+# server = Flask(__name__)
 # app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
 app = dash.Dash(__name__, 
-                server=server, 
+                # server=server, 
                 external_stylesheets=external_stylesheets,
                 url_base_pathname="/dashboard/")
+server = app.server
 
 # Defines the actual layout of HTML elements on the application
 app.layout = html.Div(children=[
