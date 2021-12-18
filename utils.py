53539ca9cef19f613002d427d4be2b6529bb1eff
koumorikun <turbohuynh@gmail.com>
2021-12-18 00:26:13 -0800

    localhost

--- utils.py
+++ utils.py
@@ -142,15 +142,19 @@ def get_ambulances():
     global base_url
     ## with open("config.yml", 'r') as ymlfile:
     ##     cfg = yaml.safe_load(ymlfile)
-    # SERVER = 'UCSD'
     # cfg = cfg[SERVER]
-    # cfg = {
-    #     "authurl" : os.environ["DASHBOARD_AUTHURL_" + SERVER],
-    #     "url" : os.environ["DASHBOARD_URL_" + SERVER],
-    #     "username" : os.environ["DASHBOARD_USERNAME_" + SERVER],
-    #     "password" : os.environ["DASHBOARD_PASSWORD_" + SERVER],
-    # }
-    base_url = "https://localhost/en/api/"
+    mode = "dev" # change to dev if working on local machine
+    if mode == "dev":
+        SERVER = 'UCSD'
+        cfg = {
+            # "authurl" : os.environ["DASHBOARD_AUTHURL_" + SERVER],
+            "url" : os.environ["DASHBOARD_URL_" + SERVER],
+            # "username" : os.environ["DASHBOARD_USERNAME_" + SERVER],
+            # "password" : os.environ["DASHBOARD_PASSWORD_" + SERVER],
+        }
+        base_url = cfg["url"]
+    elif mode == "production":
+        base_url = "https://localhost/en/api/"
     try:
         res = get('ambulance')
     except:
